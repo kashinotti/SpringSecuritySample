d42d4af15aaf8f264eb49ba98a37504c22e122fd
kashinotti <ryo.ka.notti@gmail.com>
2022-06-25 17:24:27 +0900

    ログイン画面作成、ログアウト機能実装

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**");
+	}
+	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("ryota")
+            .password(passwordEncoder().encode("123456"))
+            .roles("USER");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+    		.anyRequest()
+    		.authenticated();
+    	http.formLogin()
+    		.loginPage("/login")
+    		.defaultSuccessUrl("/")
+    		.permitAll();
+    	http.logout()
+    		.permitAll();
+    }
+}
